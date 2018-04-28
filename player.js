8b9f26b1cb40d2623c0f69e6f0bfb08c0321586e
Cameron Oglesby <oglesbycam@gmail.com>
2018-04-28 19:18:37 -0400

    final update

--- player.js
+++ player.js
@@ -1,31 +1,47 @@
-function Player(x, y, r) {
+// The Player class provides the core functions of the game
+
+// establishes a new Player, with a given (x,y) position and radius
+function Player(x, y, radius) {
   this.pos = createVector(x, y);
-  this.r = r;
+  this.radius = radius;
   this.vel = createVector(0,0);
 
-  this.update = function() {
+// the updateVel function updates the Player's new velocity . . .
+// by slowing the player down the bigger they get
+  this.updateVel = function() {
     var newvel = createVector(mouseX-width/2, mouseY-height/2);
     newvel.setMag(2.5);
     this.vel.lerp(newvel, 0.2);
     this.pos.add(this.vel);
   }
 
-  this.eats = function(other) {
+// the playerEats function applies when the player comes in contact with another entity
+  this.playerEats = function(other) {
+    // variable d is the distance between the player and another entity
     var d = p5.Vector.dist(this.pos, other.pos);
-    if (d < this.r + other.r) {
-      if (this.r >= other.r) {
-      var sum = PI * this.r * this.r + PI * other.r * other.r;
-      return this.r = sqrt(sum / PI);
-      //this.r += other.r;
+    // when the total distance is less then both radii combined, then they are in contact
+    if (d < this.radius + other.radius) {
+      // sum and new radius equation given by Daniel Shiffman
+      var sum = PI * this.radius * this.radius + PI * other.radius * other.radius;
+      this.radius = sqrt(sum / PI);
       return true;
-      }
-    } else {
+    } 
+    else {
       return false;
     }
   }
 
-  this.show = function() {
+// creates the "white food circles" within the sketch, 
+// each with their own position and diameter
+  this.showPlayers = function() {
     fill(255);
-    ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
+    ellipse(this.pos.x, this.pos.y, this.radius*2, this.radius*2);
+  }
+
+// creates the "red enemy cells" within the sketch,
+// each with their own position and diameter
+  this.showEnemy = function() {
+    fill('darkred');
+    ellipse(this.pos.x, this.pos.y, this.radius*2, this.radius*2);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,44 +1,59 @@
 var player;
 var players = [];
+var enemies = [];
 var zoom = 1;
-var img;
 
-function preload() {
-  img = loadImage("libraries/images/background.jpg");
-}
-
+// Setup creates the html canvas sketch
 function setup() {
-  createCanvas(3888, 2592);
+  createCanvas(windowWidth, windowHeight);
+  // creates you, the Player, starting at the origin w/ a radius of 20
   player = new Player(0, 0, 20);
-  for (var i = 0; i < 85; i++) {
+  // creates 85 "food cells" with random coordinates and a radius of 15
+  for (var i = 0; i <= 85; i++) {
     var x = random(-width,width);
     var y = random(-height,height);
-    players[i] = new Player(x, y, 16);
+    players[i] = new Player(x, y, 15);
+  }
+  // creates 30 "enemy cells" with random coordinates and a radius of 75
+  for (var j = 0; j <= 30; j++) {
+    var x = random(-width,width);
+    var y = random(-height,height);
+    enemies[j] = new Player(x, y, 75);
   }
 }
 
-//function windowResized() {
-  //resizeCanvas(windowWidth, windowHeight);
-//}
+// resizes the canvas as you adjust your window
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
+// the draw function essentially ties everything together and paints the canvas from the setup function
 function draw() {
-  background(img);
-
+  background('steelblue');
+  // translates the window to stay in the POV of the Player given by Daniel Shiffman
   translate(width/2, height/2);
-  var newzoom = 64 / player.r;
+  var newzoom = 64 / player.radius;
   zoom = lerp(zoom, newzoom, 0.1);
   scale(zoom);
   translate(-player.pos.x, -player.pos.y);
 
-  for (var i = players.length-1; i >=0; i--) {
-    players[i].show();
-    if (player.eats(players[i])) {
+  // creates the other players onto the screen and encorporates the eat function to kill the other players
+  for (var i = players.length-1; i>=0; i--) {
+    players[i].showPlayers();
+    if (player.playerEats(players[i])) {
       players.splice(i, 1);
     }
   }
 
-
-  player.show();
-  player.update();
+  // creates the enemies onto the screen and encorporates the eat function to kill the player
+  for (var j = enemies.length-1; j>=0; j--) {
+    enemies[j].showEnemy();
+    if (enemies[j].playerEats(player)) {
+      player.splice(0, 1);
+    }
+  }
+  // displayes the Player onto the screen and updates its Velocity as it eats other players
+  player.showPlayers();
+  player.updateVel();
 
 }
